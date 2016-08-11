fa79749e6d462cb2316cc4dbffab2c004b4e3afe
gela8 <gtl7@nau.edu>
2016-08-11 01:18:42 -0400

    started extrating download urls

--- data-raw/read_xml_dump.R
+++ data-raw/read_xml_dump.R
@@ -4,8 +4,11 @@ library(stringr); library(magrittr); library(reshape2)
 
 ## Read in solr dump------------------------------------
 
-t <- unzip('http://dlib.york.ac.uk/irisdump.zip', 
-           exdir = 'data-raw/solr_dumps/5-23-2016')
+download.file("http://dlib.york.ac.uk/irisdump.zip", 
+         destfile="data-raw/solr_dumps/5-23-2016/irisdump.zip", mode="wb") 
+unzip ("data-raw/solr_dumps/5-23-2016/irisdump.zip", 
+       exdir = "data-raw/solr_dumps/5-23-2016", overwrite = TRUE)
+
 
 
 
@@ -15,17 +18,6 @@ t <- unzip('http://dlib.york.ac.uk/irisdump.zip',
 files <- list.files('data-raw/solr_dumps/5-23-2016', pattern = '.xml') 
 Files <- files[1:6] # for testing the for loops
 
-# Separate dataframes
-
-filepath <- file.path("data-raw/solr_dumps/5-23-2016",paste(files, sep=''))
-
-
-
-for(i in files){
-  filepath <- file.path("data-raw/solr_dumps/5-23-2016",paste(i, sep=''))
-  i <- str_replace_all(i, '.xml', '_xml')
-  assign(i, xmlParse(filepath))
-}
 
 # One big list of all of the files
 
@@ -65,7 +57,8 @@ for (i in 1:length(values)) {
 }
   
 iris_meta <- values %>%
-  map(., select, contains('iris_')) 
+  map(., select, contains('iris_')) %>%
+  
 
 
 iris_metadata <- reduce(iris_meta, full_join)
@@ -73,30 +66,10 @@ iris_metadata <- reduce(iris_meta, full_join)
 
 devtools::use_data(iris_metadata, overwrite = TRUE, internal = TRUE)
 
-# For when I need to acces the file
-# "//*/file"
 
-
-ns <- data_top %>%
-  map(., getNodeSet, c("//*/arr", "//*/file", "//*/str[@name= 'PID']"))
-
-
-# Make exploration dataframes ------------------------------------------------------------------------
+# Make exploration dataframes ---------------------------------------------------------
 
 # Extract based on a list of attributes only (PID not extracted; need str for that)
-
-author <- xml_data %>%
-  map(., xpathApply, c("//*/str[@name= 'PID']",
-                       "//arr[@name= 'iris.instrument.author']"), getChildrenStrings)%>%
-  .[1:length(.)-1] %>%
-  at_depth(., 2, str_c, collapse = "; ") %>%
-  map(., as_data_frame, validate = FALSE) %>%
-  map(., set_names, c('PID', 'iris_instrument_author')) %>%
-  ldply(., data.frame)
-
-
-
-
 
 iris_instrument_author <- xml_data %>%
   map(., xpathApply, c("//arr[@name= 'iris.instrument.author']"), 
@@ -189,105 +162,36 @@ devtools::use_data(iris_participants_target_language, overwrite = TRUE)
 devtools::use_data(iris_participants_first_language, overwrite = TRUE)
 
 
-# Returns ID information  ------------------------------------------------
-PID <- xml_data %>%
-  map(., xpathApply, "//str[@name= 'PID']",xmlValue) %>%
-  unlist(.) %>%
-  data_frame(.) %>%
-  set_names(., 'PID') 
-
-has_materials <- xml_data %>%
-  map(., xpathApply, "//arr[@name= 'iris.hasmaterial']",xmlValue) %>%
-  unlist(.) %>%
-  data_frame(.) %>%
-  set_names(., 'has_materials') 
+# Create list of download urls with PIDs
 
 
+# d_pids <- data_top %>%
+#   map(., xpathSApply, c("//*/str[@name= 'PID']"), getChildrenStrings)  %>%
+#   .[1:length(.)-1]
+# 
+# 
+# url_pattern <- "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
 
-# Attempt to extract all (not working well) ---------------------------------------------------------------
+# york: 807442, 808184, 822264, 822275, 822276, 822277, 822278, 822279, 822280, 822312 (among others) are missing the file url and code mistake
 
+# This is not working----------------------------
 
-# Use this idea to pull URL and meta data from sysdata
+d_urls <- data_top %>%
+  map(., xpathSApply, c("//*/file"), xmlAttrs, append = FALSE)  %>%
+  .[1:length(.)-1] %>%
+  map(., as_data_frame, validate = FALSE) 
 
-xml_data_ext <- xml_data %>%
-  map(., xpathApply, path = c("//*/arr", "//str[@name= 'PID']"))
+d_record <- data_top %>%
+  map(., xpathSApply, c("//record"), xmlGetAttr, 'pid')  %>%
+  .[1:length(.)-1]
+  
+  # lmap_if(., is.character, str_extract_all, url_pattern) 
+v <- d_urls
 
-val <- xml_data_ext %>%
-  flatten(.) %>%
-  llply(., xmlValue)
+for (i in 1:length(v)) {
+  
+  colnames(v[[i]]) <- d_record[[i]]
 
-att <- xml_data_ext %>%
-  flatten(.) %>%
-  llply(., xmlAttrs) %>%
-  unlist(.) %>%
-  paste(.) %>%
-  unique(.)
-
-
-# Need to find a way to preserve the attribute's name! ------------------------------------------
-
-all_values <- map(xml_data, xpathApply, paste("//arr[@name= '",att,"']", sep = ""), 
-                  xmlToList) %>%
-  unlist(.)%>%
-  paste(.) %>%
-  unique(.)
-
-
-
-# Making iris_meta ----------------------------------------------
-
-## Create dataframes of query possibilities--------------------------
-xml_data_ext <- xml_data %>%
-  llply(., xpathApply, path = c("//*/arr", "//*/file", "//*/str"))
-
-val <- xml_data_ext %>%
-  flatten(.) %>%
-  llply(., xmlValue)
-
-att <- xml_data_ext %>%
-  flatten(.) %>%
-  llply(., xmlAttrs)
-
-xml_d <- lst(att, val)
-
-unique_attrs <- xml_d %>%
-  unlist(att) %>%
-  unique(.)
-
-unique_vals  <- xml_d %>%
-  unlist(val) %>%
-  unique(.)
-
-uid <- xml_d$val[which(xml_d$att == 'PID')] %>%
-  unlist(.) %>%
-  data_frame(.) %>%
-  set_names(., 'uid')
-
-iris_instrument_authors <- xml_d$val[which(xml_d$att == 'iris.instrument.author')] %>%
-  unlist(.) %>%
-  data_frame(.) %>%
-  set_names(., 'instrument_authors') 
-
-instrument_type <- xml_d$val[which(xml_d$att == 'iris.instrument.instrumentType')] %>%
-  unlist(.) %>%
-  data_frame(.) %>%
-  set_names(., 'instrument_type')
-
-instrument_research_area <- xml_d$val[which(xml_d$att == 'iris.instrument.researchArea')] %>%
-  unlist(.) %>%
-  data_frame(.) %>%
-  set_names(., 'instrument_research_area')
-
-participant_type <- xml_d$val[which(xml_d$att == 'iris.participants.participantType')] %>%
-  unlist(.) %>%
-  data_frame(.) %>%
-  set_names(., 'participant_type')
-
-iris_meta <- bind_cols(uid, iris_instrument_authors, instrument_type,
-                       instrument_research_area, participant_type)
-
-
-
-
+}
 
 
