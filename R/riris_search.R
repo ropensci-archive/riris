fa225f2f28ddcb430ef460a2524fe6450e6e7885
Geoff LaFlair <gtlaflair@gmail.com>
2018-05-03 21:53:03 -1000

    updated search

--- R/riris_search.R
+++ R/riris_search.R
@@ -1,16 +1,8 @@
 
 
-riris_search <- function(..., iris_instrument_author,
-                        iris_instrument_instrument_type,
-                        iris_instrument_research_area,
-                        iris_instrument_linguistic_target,
-                        iris_instrument_data_type,
-                        iris_participants_proficiency_learner,
-                        iris_participants_domain_of_use,
-                        iris_participants_participant_type,
-                        iris_instrument_funder,
-                        iris_participants_target_language,
-                        iris_participants_first_language){
+riris_search <- function(..., riris_author,
+                        riris_instrument,
+                        riris_participants){
   
   dots <- lazyeval::lazy_dots(...)
   if (length(dots) > 0 && any(names(dots) != "")) {
